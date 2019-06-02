dfdf5e524b71418b7d7e7a9f76138182be6a90df
ekklesia <49834176+ekklesia11@users.noreply.github.com>
2019-06-02 22:34:13 +0900

    Add files via upload

---
+++ Lotto number.js
@@ -0,0 +1,35 @@
+// JavaScript Lotto Number Pick
+// 2019-06-01 Sat
+// 45 numbers in total
+// 6 numbers + bonus number
+// ----------------------------------------------------
+
+function Lotto() {
+  let numbers = [];
+  let bonus = Math.floor((Math.random() * 45) + 1);
+  while (numbers.length !== 6) {
+    let random = Math.floor((Math.random() * 45) + 1);
+    if (!numbers.includes(random)) {
+     numbers.push(random);
+    }
+  }
+  while (numbers.includes(bonus)) {
+    bonus = Math.floor((Math.random() * 45) + 1);
+  }
+  // return 'Lucky numbers : ' + numbers + '\n' + 'Bonus : ' + bonus;
+  numbers.sort((a, b) => a - b);
+  let luckyNum = 'Lucky Numbers : ' + numbers.join(', ') + ' / Bonus : ' + bonus.toString();
+  // console.log(luckyNum);
+
+  let winner = document.createElement('li');
+  // let winNum = document.createTextNode(luckyNum);
+  // let winner = winDiv.appendChild(winNum);
+  let location = document.getElementById('list');
+  winner.innerHTML = luckyNum;
+  let newNum = location.childNodes[0];
+  // console.log(winner);
+  location.insertBefore(winner, newNum);
+
+  console.log(newNum);
+  
+}
